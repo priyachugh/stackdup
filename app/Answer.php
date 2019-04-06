1e498da8acdd4ae3b81269fcad19abdfd22b2174
priya chugh <priyachugh96@gmail.com>
2019-04-06 18:18:30 +0530

    implementing DRY principle on models

--- app/Answer.php
+++ app/Answer.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    use VotableTrait;
+    
 	protected $fillable = ['body', 'user_id'];
     public function question($value='')
     {
@@ -49,19 +51,4 @@ class Answer extends Model
             $answer->question->save();
         });
     }
-
-     public function votes() 
-    {
-         return $this->morphToMany(User::class, 'votable');
-    }  
-
-     public function upVotes() 
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    } 
-
-    public function downVotes() 
-    {
-        return $this->votes()->wherePivot('vote', - 1);
-    } 
 }
